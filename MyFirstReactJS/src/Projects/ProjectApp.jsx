6cdd3ed5b2147354176cc62cd3af98dd133cf813
Peaxmoon <sujjalboi09@gmail.com>
2025-04-12 23:46:11 +0545

    simple color change

---
+++ MyFirstReactJS/src/Projects/ProjectApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProjectApp1 from './project1/ProjectApp1'
+
+export default function ProjectApp() {
+  return (
+    <div>
+        <ProjectApp1 />
+    </div>
+  )
+}
--- MyFirstReactJS/src/main.jsx
+++ MyFirstReactJS/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router'
+import ProjectApp from './Projects/ProjectApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <ProjectApp />
   </StrictMode>
 )
