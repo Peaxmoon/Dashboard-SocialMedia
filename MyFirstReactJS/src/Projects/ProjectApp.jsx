36cda0f0c805c5638dc1c2cb4bc19985130f9f39
Peaxmoon <sujjalboi09@gmail.com>
2025-04-13 06:37:39 +0545

    for tomorrow Project

--- MyFirstReactJS/src/Projects/ProjectApp.jsx
+++ MyFirstReactJS/src/Projects/ProjectApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProjectApp1 from './project1/ProjectApp1'
+import ProjectApp2 from './project2/ProjectApp2'
 
 export default function ProjectApp() {
   return (
     <div>
-        <ProjectApp1 />
+        {/* <ProjectApp1 /> */}
+        <ProjectApp2 />
     </div>
   )
 }
---
+++ MyFirstReactJS/src/Projects/project2/ProjectApp2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectApp2() {
+  return (
+    <div>ProjectApp2</div>
+  )
+}
+// 4;24x
+export default ProjectApp2
